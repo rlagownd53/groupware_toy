72f8684643c771bf5056e0517673e31a973b3b91
zumsim <rlagownd53@naver.com>
2024-12-04 21:28:47 +0900

    first

---
+++ groupware/src/test/java/study/groupware/GroupwareApplicationTests.java
@@ -0,0 +1,13 @@
+package study.groupware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupwareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
